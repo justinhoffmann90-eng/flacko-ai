aee5f7067137c1702eb7bed7b780facc8370005c
Justin Hoffmann <justinhoffmann@Justins-Air.lan>
2026-01-25 21:06:15 -0600

    chore: update gitignore to exclude build cache

--- .next 2/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/justinhoffmann/Desktop/Flacko_AI/flacko-ai/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
