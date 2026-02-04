d226bd177b091aeb636d6a526b288a854b446dd2
trunks <trunks@trunkss-Mini.lan>
2026-02-04 16:37:42 -0600

    auth: match background color to landing page (bg-black)

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex flex-col items-center md:justify-center bg-background px-4 pt-12 md:pt-0 pb-4">
+    <div className="min-h-screen flex flex-col items-center md:justify-center bg-black px-4 pt-12 md:pt-0 pb-4">
       <div className="w-full max-w-md md:max-w-lg">
         {children}
       </div>
